da8390118b92b0f35ce362f500dadf0da381c8a4
Kennan Gauthier <KDGauthier@student.fullsail.edu>
2024-12-31 16:40:22 -0500

    Restructured project directory for vercel deployment

--- features/contact-form/test.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-console.log('EMAIL:', process.env.EMAIL);
-console.log('EMAIL_PASSWORD:', process.env.EMAIL_PASSWORD);
-console.log('SECRET_KEY:', process.env.RECAPTCHA_SECRET_KEY);
